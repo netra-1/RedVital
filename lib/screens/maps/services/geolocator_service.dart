dd7dfc063763243140493ee94c08f0b5146f5791
Shishir-69 <shishir.kan123456789@gmail.com>
2022-02-18 23:57:17 +0545

    changed places search

--- lib/screens/maps/services/geolocator_service.dart
+++ lib/screens/maps/services/geolocator_service.dart
@@ -1,5 +1,6 @@
 import 'package:geolocator/geolocator.dart';
 
+// class for geolocator service
 class GeolocatorService{
   Future<Position> getCurrentLocation() async{
     return await Geolocator.getCurrentPosition(
--- lib/screens/maps/services/places_services.dart
+++ lib/screens/maps/services/places_services.dart
@@ -3,13 +3,14 @@ import 'dart:convert' as convert;
 
 import 'package:red_vital/model/place_search.dart';
 
+// class for places search dart
 class PlacesService{
   final key = 'AIzaSyAxnrTuuRm7yftfGU--TB-YfRCsm4rC-CM';
 
   Future<List<PlaceSearch>> getAutoComplete(String search) async{
     var url =
         "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=$search&types=(cities)&key=$key";
-    var response = await http.get(Uri.parse(url));
+    var response = await http.get(Uri.parse(url)); // response var
     var json = convert.jsonDecode(response.body);
     var jsonResults = json["predictions"];
     return jsonResults.map((place) => PlaceSearch.fromJson(place)).toList();
